e05893917d39a5b0aaecb72c3338494019c61d1f
jyang872 <jyang872@gatech.edu>
2026-04-13 09:13:09 -0400

    crude 3s

--- STModel.cpp
+++ STModel.cpp
@@ -197,7 +197,7 @@ void STModel::generateLP(IloEnv* cplex_env,IloModel* cplexmodel,
     
 
     Env.options.SANDWICH_RTOL=1e-10;
-    //Env.options.SANDWICH_MAXCUT=200;
+    Env.options.SANDWICH_MAXCUT=500;
 
 
     Env.generate_cuts(this->F[this->scenario_name].size(), PF);
