1ef584dbb7fb4176f347b59351c3d5f8840483ea
jyang872 <jyang872@gatech.edu>
2026-06-23 15:32:23 -0400

    tighetee relaxation by add cuts

--- STModel.cpp
+++ STModel.cpp
@@ -222,8 +222,8 @@ void STModel::generateLP(IloEnv* cplex_env,IloModel* cplexmodel,
     }
 
 
-    // Env.options.SANDWICH_RTOL=1e-10;
-    //Env.options.SANDWICH_MAXCUT=1;
+    Env.options.SANDWICH_RTOL=1e-10;
+    Env.options.SANDWICH_MAXCUT=200;
 
                            
 
